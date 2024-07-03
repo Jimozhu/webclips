fc48268306d7c564f7587ae7e0f5e7365ecf7445
Jimozhu <jimozhu@gmail.com>
2024-07-03 17:57:10 +0800

    add location for url

--- _config.ts
+++ _config.ts
@@ -1,11 +1,12 @@
 import lume from "lume/mod.ts";
+import basePath from "lume/plugins/base_path.ts";
 import prism from "lume/plugins/prism.ts";
 import date from "lume/plugins/date.ts";
 import jsx_preact from "lume/plugins/jsx_preact.ts";
 import esbuild from "lume/plugins/esbuild.ts";
 import mdx from "lume/plugins/mdx.ts";
 
-import { full as emoji }  from "npm:markdown-it-emoji";
+import { full as emoji } from "npm:markdown-it-emoji";
 import markdownItAttrs from "npm:markdown-it-attrs";
 
 import Tweets from "./_components/Tweets.tsx";
@@ -15,9 +16,11 @@ const markdown = {
 };
 
 const site = lume({
+  location: new URL("https://myserver.com:8787/notes"),
   prettyUrls: false,
 }, { markdown });
 
+site.use(basePath());
 site.use(prism());
 site.use(date(/* Options */));
 site.use(jsx_preact());
@@ -40,6 +43,6 @@ site.use(mdx({
 }));
 
 site.ignore("webclips.ts");
-site.copy("main.css");
+site.copy("lume.css", "assets/lume.css");
 
 export default site;
